17a7789d5314a1f7cee0c5e7560da4db763e6bf3
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-17 17:39:28 +0100

    essaies avec sqlalchemy pour la connection de base de données

--- pg/data/database.py
+++ pg/data/database.py
@@ -25,3 +25,56 @@ def create_database():
                     )''')
     conn.commit()
     conn.close()
+
+
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from cryptography.fernet import Fernet
+
+# Génération d'une clé de chiffrement (à stocker en sécurité !)
+KEY = Fernet.generate_key()
+cipher = Fernet(KEY)
+
+# Création de la base de données
+from .models import Base, PasswordORM
+
+def get_database_session():
+    engine = create_engine('sqlite:///password_manager.db')
+    Base.metadata.create_all(engine)
+    return sessionmaker(bind=engine)()
+
+def save_password(username: str, service: str, password: str):
+    with get_database_session() as session:
+        encrypted_password = cipher.encrypt(password.encode()).decode()
+        new_entry = PasswordORM(
+            username=username,
+            service=service,
+            password_encrypted=encrypted_password
+        )
+        session.add(new_entry)
+        session.commit()
+
+def retrieve_passwords(username: str):
+    session = get_database_session()
+    passwords = session.query(PasswordORM).filter_by(username=username).all()
+    session.close()
+    return [
+        {
+            'service': pwd.service,
+            'password': cipher.decrypt(pwd.password_encrypted.encode()).decode(),
+            'date_added': pwd.date_added,
+            'date_updated': pwd.date_updated
+        }
+        for pwd in passwords
+    ]
+
+def modify_password(username: str, service: str, new_password: str):
+    session = get_database_session()
+    password_entry = session.query(PasswordORM).filter_by(username=username, service=service).first()
+    if password_entry:
+        password_entry.password_encrypted = cipher.encrypt(new_password.encode()).decode()
+        password_entry.date_updated = datetime.now()
+        session.commit()
+    session.close()
+
