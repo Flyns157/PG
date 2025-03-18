76d164f05d7dc6d20e7a1df3fbeecdb13f1d75c9
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-18 22:10:57 +0100

    utilisation de sqlmodèle pour l'export et l'import de Password

--- pg/services/csv.py
+++ pg/services/csv.py
@@ -1,62 +1,60 @@
 # pg.services.csv.py
 import csv
-import sqlite3
 
 from ..utils.visual import clear_screen
 from ..utils.security import encrypt_password, decrypt_password
 
+from ..data.database import engine
+from ..data.models import Password, User
 
-def export_passwords(user_id, key):
+from sqlmodel import Session, select
+from datetime import datetime
+
+
+def export_passwords(user_id):
     clear_screen()
-    conn = sqlite3.connect("password_manager.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT site, key, password, email, phone, date_added FROM password WHERE user_id = ?", (user_id,))
-    entries = cursor.fetchall()
-    conn.close()
+
+    with Session(engine) as session:
+        user: User = session.exec(select(User).where(User.id == user_id)).first()
+        if not user:
+            print("Utilisateur inconnu.")
+            return
+        
+        with open("passwords_export.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames={*Password.__fields__.keys(), "password"} - {"id", "user_id", "password_encrypted"})
+            print("Exportation en cours...")
+            writer.writeheader()
+            for password in user.passwords:
+                password_data: dict = password.model_dump(exclude=("id", "user_id"))
+                password_data["password"] = decrypt_password(password_data.pop("password_encrypted"), user.encryption_key)
+                writer.writerow(password_data)
     
-    if not entries:
-        print("Aucun mot de passe enregistré.")
-        return
-    
-    with open("passwords_export.csv", "w", newline="", encoding="utf-8") as csvfile:
-        fieldnames = ["Site", "Identifiant", "Mot de passe", "Email", "Téléphone", "Date"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for entry in entries:
-            site, site_key, encrypted_password, email, phone, date = entry
-            password = decrypt_password(encrypted_password, key)
-            writer.writerow({"Site": site, "Identifiant": site_key, "Mot de passe": password, "Email": email, "Téléphone": phone, "Date": date})
     print("Exportation réussie dans passwords_export.csv")
 
 
-def import_csv(user_id, key):
+def import_csv(user_id):
     file_path = input("Entrez le chemin du fichier CSV à importer: ")
+    clear_screen()
     
-    try:
-        with open(file_path, mode='r', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            conn = sqlite3.connect("password_manager.db")
-            cursor = conn.cursor()
-            
-            for row in reader:
-                site = row.get('Site')
-                site_key = row.get('Identifiant')
-                password = row.get('Mot de passe')
-                email = row.get('Email', None)
-                phone = row.get('Téléphone', None)
+    with Session(engine) as session:
+        user: User = session.exec(select(User).where(User.id == user_id)).first()
+        if not user:
+            print("Utilisateur inconnu.")
+            return
+    
+        try:
+            with open(file_path, mode='r', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
 
-                if site and site_key and password:  # S'assurer que les données essentielles sont présentes
-                    encrypted_password = encrypt_password(password, key)
-                    cursor.execute("""
-                        INSERT INTO password (user_id, site, key, password, email, phone) 
-                        VALUES (?, ?, ?, ?, ?, ?)
-                    """, (user_id, site, site_key, encrypted_password, email, phone))
-            
-            conn.commit()
-            conn.close()
+                for record in reader:
+                    record["password_encrypted"]=encrypt_password(record.pop("password"), user.encryption_key)
+                    # Convertir les chaînes de caractères en objets datetime (à cause du format ISO utilisé par le CSV)
+                    record["date_added"] = datetime.fromisoformat(record["date_added"])
+                    record["date_updated"] = datetime.fromisoformat(record["date_updated"])
 
-            clear_screen()
+                    session.add(Password(**record, user_id=user_id))
+                session.commit()
+
             print("Importation réussie!")
-    except Exception as e:
-        clear_screen()
-        print(f"Erreur lors de l'importation du fichier CSV: {e}")
+        except Exception as e:
+            print(f"Erreur lors de l'importation du fichier CSV: {e}")
