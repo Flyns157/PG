76045788780fd3a84e4088a91d0e2034c148c069
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-18 21:12:41 +0100

    utilisation de sqlmodele pour le  requètage de la base de données

--- pg/data/transactions/password.py
+++ pg/data/transactions/password.py
@@ -1,69 +1,115 @@
 # pg.data.transactions.password.py
 import getpass
-import sqlite3
 
 from ...utils.security import encrypt_password, decrypt_password
 from ...utils.visual import clear_screen
 
-def save_password(user_id, key):
-    site = input("Site web: ")
-    site_key = input("Identifiant du site: ")
+from sqlmodel import Session, select
+from ..database import engine
+from ..models import Password, PasswordUpdate, User
+
+
+def save_password(user_id: int):
+    with Session(engine) as session:
+        user: User = session.exec(select(User).where(User.id == user_id)).first()
+        if not user:
+            print("Utilisateur inconnu.")
+            return
+
+    url = input("Site web: ")
+    key = input("Identifiant du site: ")
     password = getpass.getpass("Mot de passe: ")
     email = input("Adresse e-mail (optionnel): ") or None
     phone = input("Numéro de téléphone (optionnel): ") or None
-    
-    encrypted_password = encrypt_password(password, key)
-    conn = sqlite3.connect("password_manager.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO password (user_id, site, key, password, email, phone) VALUES (?, ?, ?, ?, ?, ?)",
-                   (user_id, site, site_key, encrypted_password, email, phone))
-    conn.commit()
-    conn.close()
-    print("Mot de passe enregistré avec succès!")
+    clear_screen()
 
-def retrieve_passwords(user_id, key):
-    conn = sqlite3.connect("password_manager.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT site, key, password, email, phone, date_added FROM password WHERE user_id = ?", (user_id,))
-    entries = cursor.fetchall()
-    conn.close()
+    try:
+        with Session(engine) as session:
+            new_password = Password(
+                user_id=user_id,
+                url=url,
+                key=key,
+                password_encrypted=encrypt_password(
+                    password,
+                    user.encryption_key
+                ),
+                email=email,
+                phone=phone
+            )
+            session.add(new_password)
+            session.commit()
+
+        print("Mot de passe enregistré avec succès!")
+    except Exception as e:
+        print(f"Une erreur est survenue: {e}")
+
+def delete_password(user_id: int):
+    site = input("Site web à supprimer: ")
+    clear_screen()
+
+    with Session(engine) as session:
+        statement = select(Password).where(Password.user_id == user_id, Password.url == site)
+        book_to_delete = session.exec(statement)
+        if book_to_delete:
+            session.delete(book_to_delete)
+            session.commit()
+            print("Mot de passe supprimé avec succès!")
+        else:
+            print("Aucun enregistrement trouvé pour ce site.")
+
+def retrieve_passwords(user_id: int):
+    with Session(engine) as session:
+        statement = select(Password).where(Password.user_id == user_id)
+        results = session.exec(statement)
+        entries = results.all()
     
     if not entries:
         print("Aucun mot de passe enregistré.")
         return
     
     print("\nListe des mots de passe enregistrés:")
-    for entry in entries:
-        site, site_key, encrypted_password, email, phone, date = entry
-        password = decrypt_password(encrypted_password, key)
-        print(f"\nSite: {site}\nIdentifiant: {site_key}\nMot de passe: {password}\nE-mail: {email or 'Non spécifié'}\nTéléphone: {phone or 'Non spécifié'}\nDate d'ajout: {date}")
+    for password in sorted(entries, key=lambda x: x.url):
+        print(password)
 
-def modify_password(user_id, key):
+def modify_password(user_id: int):
     clear_screen()
-    site = input("Site web à modifier: ")
-    conn = sqlite3.connect("password_manager.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, site, key, password, email, phone FROM passwords WHERE user_id = ? AND site = ?", (user_id, site))
-    entry = cursor.fetchone()
-    if not entry:
-        print("Aucun enregistrement trouvé pour ce site.")
-        conn.close()
-        return
-    
-    entry_id, _, old_key, encrypted_password, old_email, old_phone = entry
-    password = decrypt_password(encrypted_password, key)
-    
-    print(f"\nIdentifiant actuel: {old_key}\nMot de passe actuel: {password}\nEmail actuel: {old_email}\nTéléphone actuel: {old_phone}")
-    
-    new_key = input(f"Nouvel identifiant ({old_key} pour conserver): ") or old_key
-    new_password = getpass.getpass("Nouveau mot de passe (laisser vide pour conserver): ") or password
-    new_email = input(f"Nouvel email ({old_email} pour conserver): ") or old_email
-    new_phone = input(f"Nouveau téléphone ({old_phone} pour conserver): ") or old_phone
-    
-    encrypted_password = encrypt_password(new_password, key)
-    cursor.execute("UPDATE password SET key = ?, password = ?, email = ?, phone = ?, date_updated = CURRENT_TIMESTAMP WHERE id = ?", (new_key, encrypted_password, new_email, new_phone, entry_id))
-    conn.commit()
-    conn.close()
+    enter = input("Site web à modifier (url ou id): ")
 
+    with Session(engine) as session:
+        if enter.isdigit():
+            statement = select(Password).where(Password.user_id == user_id, Password.id == int(enter))
+        else:
+            statement = select(Password).where(Password.user_id == user_id, Password.url == enter)
+        
+        password_to_update = session.exec(statement).first()
+        if not password_to_update:
+            print("Aucun enregistrement trouvé pour ce site.")
+            return
+    
+    password = decrypt_password(password_to_update.password_encrypted, password_to_update.user.encryption_key)
+
+    print("Laissez un champ vide pour conserver les anciennes valeurs.\n")
+    password_updated = PasswordUpdate(
+        key=input(f"Nouvel identifiant (actuellement: {password_to_update.key} ): ") or None,
+        password_encrypted=encrypt_password(
+            getpass.getpass(f"Nouveau mot de passe (actuellement: {password} ): "),
+            password_to_update.user.encryption_key
+        ) or None,
+        email=input(f"Nouvel email (actuellement: {password_to_update.email} ") or None,
+        phone=input(f"Nouveau téléphone (actuellement: {password_to_update.phone} ): ") or None
+    )
+    
     clear_screen()
-    print("Informations mises à jour avec succès!")
+    try:
+        with Session(engine) as session:
+            update_infos=password_updated.model_dump()
+            for k, v in update_infos.items():
+                if v is not None:
+                    setattr(password_to_update, k, v)
+            session.add(password_to_update)
+            session.commit()
+            session.refresh(password_to_update)
+
+        print("Informations mises à jour avec succès!")
+    except Exception as e:
+        print(f"Une erreur est survenue: {e}")
