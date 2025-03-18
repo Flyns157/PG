7cd4a345e9a3dc2944ad16ddf7eaa8c795939c7e
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-18 14:06:19 +0100

    modification du module de models pour correspondre à l'utilisation de sqlmodel

--- pg/data/models/__init__.py
+++ pg/data/models/__init__.py
@@ -1,29 +1,19 @@
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    ...
-
 from .user import (
-    UserORM, 
     User, 
     UserCreate, 
     UserUpdate
 )
 from .password import (
-    PasswordORM, 
     PasswordCreate, 
     PasswordUpdate, 
     Password
 )
 
 __all__ = [
-    'UserORM', 
     'User', 
     'UserCreate', 
     'UserUpdate', 
-    'PasswordORM', 
     'PasswordCreate', 
     'PasswordUpdate', 
-    'Password', 
-    'Base'
+    'Password'
 ]
