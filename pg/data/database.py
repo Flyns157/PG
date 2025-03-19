b3a486a99d54ca3c7a7cfb9a8e6b82f12c3a5a99
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-19 14:10:44 +0100

    suppression de la fonction create_database

--- pg/data/database.py
+++ pg/data/database.py
@@ -1,33 +1,5 @@
 # pg.data.database.py
-import sqlite3
 from typing_extensions import deprecated
-
-@deprecated("Use sqlmodel instead")
-def create_database():
-    conn = sqlite3.connect("password_manager.db")
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS user (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        username TEXT UNIQUE NOT NULL,
-                        password_hash TEXT NOT NULL,
-                        hash_algorithm TEXT NOT NULL,
-                        encryption_key TEXT NOT NULL
-                    )''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS password (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        user_id INTEGER,
-                        site TEXT NOT NULL,
-                        key TEXT NOT NULL,
-                        password TEXT NOT NULL,
-                        email TEXT,
-                        phone TEXT,
-                        date_added TEXT DEFAULT CURRENT_TIMESTAMP,
-                        date_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                        FOREIGN KEY (user_id) REFERENCES users(id)
-                    )''')
-    conn.commit()
-    conn.close()
-
 
 from sqlmodel import create_engine, Session, SQLModel
 from sqlmodel.main import is_table_model_class
