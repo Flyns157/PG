ada6001afbce08c4b2faaa9acd2708fcbb04a2f6
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-19 14:37:57 +0100

    ajout de propriétées au modèle Password pour simplifier les interractions avec les mots de passes

--- pg/data/models/password.py
+++ pg/data/models/password.py
@@ -7,14 +7,18 @@ from pydantic_extra_types.phone_numbers import PhoneNumber
 from pydantic import EmailStr, ValidationError, HttpUrl
 from datetime import datetime
 
-from sqlmodel import SQLModel, Field, Relationship, Column
+from sqlmodel import SQLModel, Field, Relationship, Column, Session, select
+from sqlalchemy import Engine
 
 from ...utils.debugging import AutoStrRepr
 from ...utils.type import HttpUrlType
+from ...utils.security import encrypt_password, decrypt_password
+
+from ..database import engine, execute
 
 
 class PasswordBase(SQLModel, AutoStrRepr):
-    user_id: int = Field(foreign_key="user.id", description="Identifiant de l'utilisateur")
+    user_id: int = Field(foreign_key="user.id", description="Identifiant de l'utilisateur", allow_mutation=False)
     url: HttpUrl = Field(sa_column=Column(HttpUrlType), description="URL du site / service")
     description: str | None = Field(None, description="Description par l'utilisateur du site / service")
     key: str = Field(description="Clé / identifiant")
@@ -30,28 +34,36 @@ class Password(PasswordBase, table=True):
     # Relationship to the User model, setting up a one-to-many relationship
     user: "User" = Relationship(back_populates="passwords")
 
-    def __init__(self, **data):
-        try:
-            validated_data = PasswordBase.model_validate(data)
-            super().__init__(**validated_data.model_dump())
-        except ValidationError as e:
-            raise ValueError(f"Invalid data: {e}")
+    @property
+    def loaded_user(self):
+        """
+        Retourne l'utilisateur complet (contournement de l'import paresseux depuis la base de données)
+        """
+        from ..models.user import User
+        if isinstance(self.user, User):  # Si l'utilisateur est déjà chargé, on le retourne directement
+            return self.user
+        return User.get_by_id(self.user_id)
+    
+    @property
+    def password(self):
+        """
+        Retourne le mot de passe en clair
+        """
+        return decrypt_password(self.password_hash, self.loaded_user.encryption_key)
+    
+    @password.setter
+    def password(self, password: str):
+        """
+        Définit le mot de passe en clair
+        """
+        self.password_hash = encrypt_password(password, self.loaded_user.encryption_key)
     
     def __str__(self):
-        from ...utils.security import decrypt_password
-        from ..database import engine
-        from .import User
-
-        from sqlmodel import Session, select
-        with Session(engine) as session:
-            statement = select(User).where(User.id == self.user_id)
-            user = session.exec(statement).first()
-
         return f"\n" \
                 f"Site web: {self.url} (id: {self.id})\n" \
                 f"Description: {self.description or 'Non spécifié'}\n" \
                 f"Identifiant: {self.key}\n" \
-                f"Mot de passe: {decrypt_password(self.password_encrypted, user.encryption_key)}\n" \
+                f"Mot de passe: {self.password}\n" \
                 f"Email: {self.email or 'Non spécifié'}\n" \
                 f"Téléphone: {(self.phone or '    Non spécifié')[4:]}\n" \
                 f"Date de création: {self.date_added}\n" \
