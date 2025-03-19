43465cb2a4abc657d24f9653dff3da68fbc798c3
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-19 11:06:31 +0100

    ajout d'une fonction interact qui gère automatiquement la création de session

--- pg/data/database.py
+++ pg/data/database.py
@@ -1,6 +1,7 @@
 # pg.data.database.py
 import sqlite3
 
+@DeprecationWarning
 def create_database():
     conn = sqlite3.connect("password_manager.db")
     cursor = conn.cursor()
@@ -27,7 +28,30 @@ def create_database():
     conn.close()
 
 
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
+from sqlmodel.orm.session import Select, _TSelectParam
+from sqlalchemy import Engine
 
 
 engine = create_engine('sqlite:///password_manager.db')
+
+from enum import Enum
+
+class FetchMode(Enum):
+    ALL = 1
+    ONE = 2
+
+def interact(statement: Select[_TSelectParam], engine: Engine = engine, session: Session | None = None, fetch_mode: FetchMode = FetchMode.ONE):
+    if (must_be_closed := session is None):
+        session = Session(engine)
+    tmp=session.exec(statement)
+
+    match fetch_mode:
+        case FetchMode.ALL:
+            result = list(tmp.all())
+        case FetchMode.ONE:
+            result = tmp.first()
+    
+    if must_be_closed:
+        session.close()
+    return result
