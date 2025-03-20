f2da9e43d2833a72731c2be862bf205bc6ad155e
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-20 19:11:41 +0100

    ajout de fonctions d'import et d'export en csv dans les services

--- pg/services/password.py
+++ pg/services/password.py
@@ -1,11 +1,14 @@
 # pg.services.password.py
 from typing_extensions import deprecated
+from pathlib import Path
+import csv
+from datetime import datetime
 
 from ..utils.visual import clear_screen
 from ..utils.search_engine import similar_passwords
 
 from ..data.database import engine
-from ..data.models import User
+from ..data.models import User, Password
 
 from sqlmodel import Session, select
 
@@ -24,3 +27,33 @@ def search_password(user_id):
 
     print(f"""\nInformations de connections similaires à la recherche\n\n-> Recherche: "{search_term}"\n""")
     print(nearest_password)
+
+def import_passwords(user: User, file_path: str | Path, session: Session=None) -> None:
+    with open(file_path, mode='r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for record in reader:
+            Password.create(
+                session=session,
+                user_id=user.id,
+                url=record["url"],
+                description=record["description"],
+                key=record["key"],
+                password=record["password"],
+                email=record["email"],
+                phone=record["phone"],
+                # Convertion les chaînes de caractères en objets datetime (à cause du format ISO utilisé par le CSV)
+                date_added=datetime.fromisoformat(record["date_added"]),
+                date_updated=datetime.fromisoformat(record["date_updated"])
+            )
+
+def export_passwords(user: User, file_path: str | Path, session: Session=None) -> None:
+    user = User.get_by_id(user.id, session=session)
+    
+    with open(file_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames={*Password.__fields__.keys(), "password"} - {"id", "user_id", "password_encrypted"})
+        writer.writeheader()
+        for password in user.passwords:
+            password_data: dict = password.model_dump(exclude=("id", "user_id", "password_encrypted"))
+            password_data["password"] = password.password
+            writer.writerow(password_data)
