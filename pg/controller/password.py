59cd7851cce6bf8bc72943fd2e660e0fae0228e0
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-20 19:12:27 +0100

    utilisation des nouvelles fonctions d'import/export des mot de passes dans le le controller de Password

--- pg/controller/password.py
+++ pg/controller/password.py
@@ -10,6 +10,8 @@ from ..utils.search_engine import similar_passwords
 from ..data.database import engine
 from ..data.models import Password, User
 
+from ..services.password import import_passwords, export_passwords
+
 
 def create_password(user: User):
     clear_screen()
@@ -108,57 +110,21 @@ def search_password(user: User):
 def export_passwords(user: User):
     clear_screen()
     try:
-        with Session(engine) as session:
-            user = User.get_by_id(user.id, session=session)
-            
-            with open("passwords_export.csv", "w", newline="", encoding="utf-8") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames={*Password.__fields__.keys(), "password"} - {"id", "user_id", "password_encrypted"})
-                print("Exportation en cours...")
-                writer.writeheader()
-                for password in user.passwords:
-                    password_data: dict = password.model_dump(exclude=("id", "user_id", "password_encrypted"))
-                    password_data["password"] = password.password
-                    writer.writerow(password_data)
-    
-        print("Exportation réussie dans passwords_export.csv")
+        file_path = input("Entrez le chemin du fichier CSV à exporter: ") or "passwords_export.csv"
+        export_passwords(user, file_path)
+
+        clear_screen()
+        print("Exportation réussie dans passwords_export.csv", end="\n\n")
     except Exception as e:
-        print(f"Une erreur est survenue: {e}", end="\n\n")
-    finally:
-        try:
-            session.close()
-        except Exception:
-            pass
+        print(f"Erreur lors de l'exportation des mots de passes: {e}", end="\n\n")
 
 def import_passwords(user: User):
     file_path = input("Entrez le chemin du fichier CSV à importer: ")
     clear_screen()
-    
-    with Session(engine) as session:
-        user = User.get_by_id(user.id, session=session)
-        if not user:
-            print("Utilisateur inconnu.")
-            return
-    
-        try:
-            with open(file_path, mode='r', encoding='utf-8') as csvfile:
-                reader = csv.DictReader(csvfile)
+    try:
+        import_passwords(user, file_path)
 
-                for record in reader:
-                    Password.create(
-                        session=session,
-                        user_id=user.id,
-                        url=record["url"],
-                        description=record["description"],
-                        key=record["key"],
-                        password=record["password"],
-                        email=record["email"],
-                        phone=record["phone"],
-                        # Convertion les chaînes de caractères en objets datetime (à cause du format ISO utilisé par le CSV)
-                        date_added=datetime.fromisoformat(record["date_added"]),
-                        date_updated=datetime.fromisoformat(record["date_updated"])
-                    )
-
-            clear_screen()
-            print("Importation réussie!")
-        except Exception as e:
-            print(f"Erreur lors de l'importation du fichier CSV: {e}")
+        clear_screen()
+        print("Importation réussie!", end="\n\n")
+    except Exception as e:
+        print(f"Erreur lors de l'importation du fichier CSV: {e}", end="\n\n")
