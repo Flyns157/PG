cc936be1dff8f2dbee859fc4da18e90bcc72903c
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-20 19:46:19 +0100

    mise à jour de l'écran de connection

--- pg/controller/auth.py
+++ pg/controller/auth.py
@@ -10,13 +10,17 @@ def connect():
     while True:
         print("\n1. S'inscrire\n2. Se connecter\n3. Quitter")
         choice = input("Choisissez une option: ")
-        if choice == "1":
-            from .user import create_user
-            create_user()
-        elif choice == "2":
-            login()
-        elif choice == "3":
-            break
+        match choice:
+            case "1":
+                from .user import create_user
+                create_user()
+            case "2":
+                login()
+            case "3":
+                break
+            case _:
+                clear_screen()
+                print("Option invalide.\n")
 
 def login():
     clear_screen()
