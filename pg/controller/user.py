d26925d09162e4ea1677c71364e9d7ab49d5524d
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-20 17:42:20 +0100

    ajout d'un controller pour les utilisateurs

---
+++ pg/controller/user.py
@@ -0,0 +1,34 @@
+# pg.controller.user.py
+import getpass
+from sqlmodel import Session
+
+from ..utils.visual import clear_screen
+from ..utils.security import supported_algorithms
+
+from ..data.database import engine
+from ..data.models import User
+
+from .home import home
+
+
+def create_user():
+    clear_screen()
+    try:
+        with Session(engine) as session:
+            user = User.create(
+                session=session,
+                username=input("Nom d'utilisateur: "),
+                email=input("Adresse e-mail: "),
+                password=getpass.getpass("Mot de passe: "),
+                hash_algorithm=input(f"\nAlgorithme de hashage à utiliser\n-> {', '.join(supported_algorithms())}\n(optionnel): ") or "sha256"
+            )
+            clear_screen()
+            print(f"Utilisateur {user.username} créé avec succès!")
+            home(user)
+    except Exception as e:
+        print(f"Une erreur est survenue: {e}")
+    finally:
+        try:
+            session.close()
+        except Exception:
+            pass
