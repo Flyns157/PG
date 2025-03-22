3b0922408ebc7f811a7deda00fea3ebfe9a1e215
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-22 13:37:25 +0100

    ajout d'un paramètre de ;ancement de l'application pour choisir le mode d'affichage

--- pg/__init__.py
+++ pg/__init__.py
@@ -1,5 +1,7 @@
 # pg.__init__.py
 from sqlmodel import SQLModel
+import tkinter as tk
+from enum import StrEnum
 
 from .utils.visual import clear_screen
 
@@ -7,13 +9,29 @@ from .data.database import engine
 from .data.models import *
 
 from .controller.auth import connect
+from .view import PasswordGestionApp
 
 
 def init():
-    clear_screen()
-    print("Bienvenue dans Password Gestion!")
     SQLModel.metadata.create_all(engine)
 
-def run_app():
+class Mode(StrEnum):
+    CONSOLE = "console"
+    GUI = "gui"
+
+def run_app(mode: Mode = Mode.GUI):
     init()
+    if mode == Mode.GUI:
+        run_gui()
+    else:
+        run_console()
+
+def run_gui():
+    root = tk.Tk()
+    app = PasswordGestionApp(root)
+    root.mainloop()
+
+def run_console():
+    clear_screen()
+    print("Bienvenue dans Password Gestion!")
     connect()
