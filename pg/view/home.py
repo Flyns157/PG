4aa19b2849aa3501f58cf6f4e8be1acd235b45bd
Mattéo Cuisset <matteo.cuisset@gmail.com>
2025-03-25 14:08:44 +0100

    ajoustement de l'interface graphique

--- pg/view/home.py
+++ pg/view/home.py
@@ -9,12 +9,15 @@ from . import clear_screen
 from .password import add_password_tree, load_passwords, toggle_password_visibility
 
 
+NOT_IMPLEMENTED_YET = "Fonctionnalité à implémenter"
+
 def create_home_screen(root: Tk, user: User):
     clear_screen(root=root)
     
     menu = Frame(root)
     menu.pack(fill="x")
     
+    Button(menu, text="Importer/Exporter", command=lambda: messagebox.showinfo("Import/Export", NOT_IMPLEMENTED_YET)).pack(side="left", padx=5, pady=5)
     Button(menu, text="Déconnexion", command=lambda: create_login_screen(root)).pack(side="right", padx=5, pady=5)
     Button(menu, text="À propos", command=lambda: messagebox.showinfo("À propos", "Gestionnaire de mots de passe v1.0")).pack(side="right", padx=5, pady=5)
     
@@ -45,6 +48,6 @@ def create_home_screen(root: Tk, user: User):
     button_frame = Frame(root)
     button_frame.pack(pady=5)
     
-    Button(button_frame, text="Ajouter un mot de passe", command=lambda: messagebox.showinfo("Ajout", "Fonctionnalité à implémenter")).pack(side="left", padx=5)
-    Button(button_frame, text="Exporter", command=lambda: messagebox.showinfo("Export", "Exportation en cours...")).pack(side="left", padx=5)
-    Button(button_frame, text="Afficher/Masquer mots de passe", command=lambda: toggle_password_visibility(tree)).pack(side="left", padx=5)
+    Button(button_frame, text="Ajouter un mot de passe", command=lambda: messagebox.showinfo("Ajout", NOT_IMPLEMENTED_YET)).pack(side="left", padx=5)
+    Button(button_frame, text="Modifier le mot de passe", command=lambda: messagebox.showinfo("Modification", NOT_IMPLEMENTED_YET)).pack(side="left", padx=5)
+    Button(button_frame, text="Afficher/Masquer mot de passe", command=lambda: toggle_password_visibility(tree)).pack(side="left", padx=5)
